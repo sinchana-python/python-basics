131796138e1343db8e323b0ac39de3011b481650
sinchana-python <s2756975@gmail.com>
2026-03-02 22:49:30 +0530

    Add files via upload

---
+++ day4_loops.py
@@ -0,0 +1,21 @@
+#print 1 to 10 using for loop
+for i in range ( 1,11):
+    print(i)
+
+#multiplication program
+print ("enter a number:")
+n=int(input())
+i=1
+while (i<=10):
+    ans=n*i
+    print(n,"*",i,"=",ans)
+    i=i+1
+
+#sum of N numbers
+    print("enter a number:")
+    n=int(input())
+    total=0
+    for i in range(1,n + 1):
+        total=total+i
+        print ("sum is:",total)
+
